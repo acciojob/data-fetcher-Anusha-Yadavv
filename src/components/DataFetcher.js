c81c2a47f4e25cc7a42fb86a1c94da5b4079d59e
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-22 14:01:16 +0530

    api

---
+++ src/components/DataFetcher.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+const DataFetcher = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Data from API:</h2>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
+    </div>
+  );
+};
+
+export default DataFetcher;
